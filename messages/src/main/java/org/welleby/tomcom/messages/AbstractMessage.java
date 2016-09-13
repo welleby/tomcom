0ec9c6dc4616aabc5b304d75fb160b9ee42f7a75
welleby <tommywelleby@gmail.com>
2016-09-13 12:51:37 +0200

    Moved toBytes from SensorReport to AbstractMessage

--- messages/src/main/java/org/welleby/tomcom/messages/AbstractMessage.java
+++ messages/src/main/java/org/welleby/tomcom/messages/AbstractMessage.java
@@ -1,6 +1,12 @@
 package org.welleby.tomcom.messages;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
+import com.google.common.primitives.Shorts;
 
 public abstract class AbstractMessage extends Header {
 
@@ -8,4 +14,28 @@ public abstract class AbstractMessage extends Header {
 		super(messageType);
 	}
 	public abstract List<Byte> getBytes() throws MessageParsingException;
+	
+	public List<Byte> toBytes(Object value) throws MessageParsingException{
+		List<Byte> result = new ArrayList<>();
+		if(value instanceof Byte){
+			result.add((Byte)value);
+		}
+		else if(value instanceof Short){
+			byte[] byteArray = Shorts.toByteArray((Short)value);
+			result.addAll(Bytes.asList(byteArray));
+		}
+		else if(value instanceof Integer){
+			byte[] byteArray = Ints.toByteArray((Integer)value);
+			result.addAll(Bytes.asList(byteArray));
+		}
+		else if(value instanceof Long){
+			byte[] byteArray = Longs.toByteArray((Long)value);
+			result.addAll(Bytes.asList(byteArray));
+		}
+		
+		if(result.size()==0)
+			throw new MessageParsingException("Could not convert value to bytes " + value);
+		
+		return result;
+	}
 }
--- messages/src/main/java/org/welleby/tomcom/messages/SensorReport.java
+++ messages/src/main/java/org/welleby/tomcom/messages/SensorReport.java
@@ -4,11 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.common.primitives.Bytes;
-import com.google.common.primitives.Ints;
-import com.google.common.primitives.Longs;
-import com.google.common.primitives.Shorts;
-
 public class SensorReport extends AbstractMessage{
 
 	private Map<String,Object> values = new HashMap<>();
@@ -21,21 +16,7 @@ public class SensorReport extends AbstractMessage{
 	public List<Byte> getBytes() throws MessageParsingException {
 		List<Byte> result = getHeaderBytes();
 		for(Object value : values.values()){
-			if(value instanceof Byte){
-				result.add((Byte)value);
-			}
-			else if(value instanceof Short){
-				byte[] byteArray = Shorts.toByteArray((Short)value);
-				result.addAll(Bytes.asList(byteArray));
-			}
-			else if(value instanceof Integer){
-				byte[] byteArray = Ints.toByteArray((Integer)value);
-				result.addAll(Bytes.asList(byteArray));
-			}
-			else if(value instanceof Long){
-				byte[] byteArray = Longs.toByteArray((Long)value);
-				result.addAll(Bytes.asList(byteArray));
-			}
+			result.addAll(toBytes(value));
 		}
 		return result;
 	}
